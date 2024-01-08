164ed0ee85b622f2f1745dbe78b0b241956b6716
jods4 <joel.dumas@elca.ch>
2024-01-09 00:56:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { routes } from 'vue-router/auto/routes'
+</script>
+
+<template>
+  <main>
+    <div v-for="r of routes">
+      <div v-for="(value, key) of r.meta">
+        <b>{{ key }}</b> = {{ value }}
+      </div>
+    </div>
+  </main>
+</template>
---
+++ src/Pages/index.vue
@@ -0,0 +1,7 @@
+<route lang="yaml">
+  meta:
+    title: Home 3
+    description: This is the home page
+</route>
+
+<template></template>
